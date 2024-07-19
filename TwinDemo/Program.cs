19e4c5fd4dcd78d897c0f2fa85db079466725b73
Bert Haddad <bhaddad5@gmail.com>
2024-07-19 13:26:38 -0700

    Created Cavrnus digital twin simulation

---
+++ TwinDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿using CavrnusSdk.API;
+using Twin.Connector;
+
+internal class Program
+{
+	private static Twin_Highway_Fake highway;
+
+	private static async Task Main(string[] args)
+	{
+		Console.WriteLine("Please enter your domain:");
+		string server = Console.ReadLine();
+		Console.WriteLine("Starting up Cavrnus...");
+
+		CavrnusFunctionLibrary.InitializeCavrnus(false);
+
+		//Console.WriteLine("Type your server:");
+		string userName = "Twin Data Connector";
+
+		var authRes = await CavrnusFunctionLibrary.AuthenticateAsGuest(server, userName);
+
+		if (!authRes.IsOk)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"Failed to log in: {authRes.Error}");
+			Console.ResetColor();
+			Console.ReadLine();
+			return;
+		}
+
+		Console.WriteLine("Please enter space Join ID:");
+		string spaceJoinId = Console.ReadLine();
+
+		Console.WriteLine("Authenticated, connecting to space...");
+
+		var spaceConnRes = await CavrnusFunctionLibrary.JoinSpace(spaceJoinId);
+
+		if (!spaceConnRes.IsOk)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"Failed to join space: {spaceConnRes.Error}");
+			Console.WriteLine($"Are you sure you created a space with Join ID: \"twin-demo\"?");
+			Console.ResetColor();
+			Console.ReadLine();
+			return;
+		}
+		var spaceConn = spaceConnRes.Value;
+
+		Console.WriteLine("Connection Complete!");
+
+		highway = new Twin_Highway_Fake();
+
+		PollAndSend(spaceConn);
+
+		Console.WriteLine("Data now sending data from virtual sensors...");
+
+		Console.WriteLine("INSTRUCTIONS:");
+		Console.WriteLine("\tType 'crisis'+Enter to set the value of a sensor to emergency levels.");
+		Console.WriteLine("\tType 'solve'+Enter to \"fix\" the issue and set the levels back to their normal range.");
+
+		while (true)
+		{
+			string cmd = Console.ReadLine();
+			highway.SendCommand(cmd);
+		}
+
+	}
+
+	private static async Task PollAndSend(CavrnusSpaceConnection spaceConn)
+	{
+		await Task.Delay(1000);
+
+		//Console.WriteLine("Polling and Sending");
+
+		foreach (var sensor in highway.AllSensors)
+		{
+			if (sensor is TwinAirSensor airSensor)
+			{
+				spaceConn.PostBoolPropertyUpdate(airSensor.Id, "Crisis", airSensor.Crisis);
+
+				spaceConn.PostFloatPropertyUpdate(airSensor.Id, "CO-ppm", (float)airSensor.CurrCOppm);
+				//Console.WriteLine($"Posting {airSensor.Id} CO {airSensor.CurrCOppm} ppm");
+				spaceConn.PostFloatPropertyUpdate(airSensor.Id, "NO2-ppb", (float)airSensor.CurrNO2ppb);
+				//Console.WriteLine($"Posting {airSensor.Id} NO2 {airSensor.CurrNO2ppb} ppb");
+			}
+			if (sensor is TwinTempSensor tempSensor)
+			{
+				spaceConn.PostBoolPropertyUpdate(tempSensor.Id, "Crisis", tempSensor.Crisis);
+
+				spaceConn.PostFloatPropertyUpdate(tempSensor.Id, "Temp-C", (float)tempSensor.CurrTemperatureC);
+				//Console.WriteLine($"Posting {tempSensor.Id} Temperature {tempSensor.CurrTemperatureC} C");
+			}
+		}
+
+		PollAndSend(spaceConn);
+	}
+}
---
+++ TwinDemo/Twin_Highway.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twin.Connector
+{
+	public class Twin_Highway_Fake
+	{
+		public List<ITwinSensor> AllSensors;
+
+
+		public Twin_Highway_Fake()
+		{
+			AllSensors = new List<ITwinSensor>()
+			{
+					new TwinTempSensor("T1-SB-TEMP-1"),
+					new TwinTempSensor("T1-SB-TEMP-2"),
+					new TwinTempSensor("T1-SB-TEMP-3"),
+
+					new TwinAirSensor("T1-SB-AIR-1"),
+					new TwinAirSensor("T1-SB-AIR-2"),
+					new TwinAirSensor("T1-SB-AIR-3"),
+			};
+
+			UpdateCurrentData();
+		}
+
+		public void SendCommand(string cmd)
+		{
+			if (cmd == "crisis")
+			{
+				Random rnd = new Random();
+				AllSensors[rnd.Next(0, AllSensors.Count - 1)].Crisis = true;
+			}
+			if (cmd == "solve")
+			{
+				foreach (var sensor in AllSensors)
+				{
+					if (sensor.Crisis)
+					{
+						sensor.Crisis = false;
+						break;
+					}
+				}
+			}
+
+			foreach (var sensor in AllSensors)
+			{
+				sensor.UpdateData();
+			}
+		}
+
+		private async Task UpdateCurrentData()
+		{
+			await Task.Delay(3000);
+
+			foreach (var sensor in AllSensors)
+			{
+				sensor.UpdateData();
+			}
+
+			UpdateCurrentData();
+		}
+	}
+
+	public interface ITwinSensor
+	{
+		string Id { get; }
+		bool Crisis { get; set; }
+
+		void UpdateData();
+	}
+
+	public class TwinTempSensor : ITwinSensor
+	{
+		public bool Crisis { get; set; }
+		public string Id => id;
+		private string id;
+
+		public TwinTempSensor(string id)
+		{
+			this.id = id;
+			UpdateData();
+		}
+
+		public double CurrTemperatureC;
+
+		double minNormalTemp = 25;
+		double maxNormalTemp = 28;
+
+		double minCrisisTemp = 44;
+		double maxCrisisTemp = 46;
+
+		public void UpdateData()
+		{
+			if (Crisis)
+				CurrTemperatureC = DataUpdateHelpers.WobbleNumberInRange(CurrTemperatureC, minCrisisTemp, maxCrisisTemp, .2f);
+			else
+				CurrTemperatureC = DataUpdateHelpers.WobbleNumberInRange(CurrTemperatureC, minNormalTemp, maxNormalTemp, .2f);
+		}
+	}
+
+	public class TwinAirSensor : ITwinSensor
+	{
+		public bool Crisis { get; set; }
+		public string Id => id;
+		private string id;
+
+		public TwinAirSensor(string id)
+		{
+			this.id = id;
+			UpdateData();
+		}
+
+		public double CurrCOppm;
+		public double CurrNO2ppb;
+
+		double minNormalCO = 2;
+		double maxNormalCO = 4;
+
+		double minCrisisCO = 8;
+		double maxCrisisCO = 10;
+
+		double minNormalNO2 = 67;
+		double maxNormalNO2 = 70;
+
+
+		public void UpdateData()
+		{
+			if (Crisis)
+				CurrCOppm = DataUpdateHelpers.WobbleNumberInRange(CurrCOppm, minCrisisCO, maxCrisisCO, .2f);
+			else
+				CurrCOppm = DataUpdateHelpers.WobbleNumberInRange(CurrCOppm, minNormalCO, maxNormalCO, .2f);
+
+			CurrNO2ppb = DataUpdateHelpers.WobbleNumberInRange(CurrNO2ppb, minNormalNO2, maxNormalNO2, .2f);
+		}
+	}
+
+	public static class DataUpdateHelpers
+	{
+		private static Random r = new Random();
+		public static double GetRandomNumberInRange(double minNumber, double maxNumber)
+		{
+			return r.NextDouble() * (maxNumber - minNumber) + minNumber;
+		}
+
+		public static double WobbleNumberInRange(double currValue, double minNumber, double maxNumber, double step)
+		{
+			if (currValue < minNumber || currValue > maxNumber)
+				return GetRandomNumberInRange(minNumber, maxNumber);
+
+			var adjustedStep = GetRandomNumberInRange(step - .1, step + .1);
+			if (r.Next(0, 2) > 0)
+				currValue += adjustedStep;
+			else
+				currValue -= adjustedStep;
+
+			return Math.Min(maxNumber, Math.Max(minNumber, currValue));
+		}
+	}
+}
